f91527a9a0823cf9f633d7c0bd94e45a8eaaca20
DenisZackharov <denis.zaharov@flatstack.com>
2021-07-27 21:19:14 +0300

    Fix problem in comments too

--- db/migrate/20210722172410_devise_create_users.rb
+++ db/migrate/20210722172410_devise_create_users.rb
@@ -6,8 +6,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :firstname
       t.string :lastname
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false
+      t.string :encrypted_password, null: false
 
       ## Recoverable
       t.string   :reset_password_token
