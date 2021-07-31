51af57f5a69ee3675598c8e9e4cae7df1d845f90
DenisZackharov <denis.zaharov@flatstack.com>
2021-07-31 21:40:40 +0300

    add and configure database_cleaner

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    firstname
-    lastname
-    email
-    password
+    firstname { 'Denis' }
+    lastname { 'Zaharov' }
+    email { 'denis.zaharov@flatstack.com' }
+    password { '100100' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'validations' do
+    let(:user) { FactoryBot.build(:user) }
 
+    it 'is a firstname' do
+      user.firstname = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'is a lastname' do
+      user.lastname = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'is a email' do
+      user.email = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'is a password' do
+      user.password = nil
+      expect(user).not_to be_valid
+    end
+  end
 end
