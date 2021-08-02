fbdd53c4f1e608bd3006e5fce0c6f712af0b4957
DenisZackharov <denis.zaharov@flatstack.com>
2021-08-02 19:25:27 +0300

    change single quotes rubocop to double quotes

--- test/controllers/preinternship_controller_test.rb
+++ test/controllers/preinternship_controller_test.rb
@@ -1,12 +1,12 @@
-require 'test_helper'
+require "test_helper"
 
 class PreinternshipControllerTest < ActionDispatch::IntegrationTest
-  test 'should get home' do
+  test "should get home" do
     get preinternship_home_url
     assert_response :success
   end
 
-  test 'should get about' do
+  test "should get about" do
     get preinternship_about_url
     assert_response :success
   end
