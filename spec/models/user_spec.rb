a77b96e89b51d6bd137d79136ecc86e6df487c15
DenisZackharov <denis.zaharov@flatstack.com>
2021-08-04 00:35:23 +0300

    add rubocop changes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,25 +1,25 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  describe 'validations' do
+  describe "validations" do
     let(:user) { FactoryBot.build(:user) }
 
-    it 'is a firstname' do
+    it "is a firstname" do
       user.firstname = nil
       expect(user).not_to be_valid
     end
 
-    it 'is a lastname' do
+    it "is a lastname" do
       user.lastname = nil
       expect(user).not_to be_valid
     end
 
-    it 'is a email' do
+    it "is a email" do
       user.email = nil
       expect(user).not_to be_valid
     end
 
-    it 'is a password' do
+    it "is a password" do
       user.password = nil
       expect(user).not_to be_valid
     end
