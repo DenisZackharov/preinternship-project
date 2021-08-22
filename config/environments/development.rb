cd7eef0e6c9bbf0f86b4b022d7896f743541826d
DenisZackharov <denis.zaharov@flatstack.com>
2021-08-22 22:09:35 +0300

    add production env key, add config for forgot password, but still not working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,15 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: "04a98414f0c00e",
+    password: "02095041adc22f",
+    address: "smtp.mailtrap.io",
+    domain: "smtp.mailtrap.io",
+    port: "2525",
+    authentication: :cram_md5
+  }
+
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 end
