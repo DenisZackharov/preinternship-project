6e77b460d7f8e37dd324db1f65581c342ebffb9f
DenisZackharov <denis.zaharov@flatstack.com>
2021-08-23 21:08:05 +0300

    add forgot password and confirmation instructions features

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,14 +75,9 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name: "04a98414f0c00e",
-    password: "02095041adc22f",
-    address: "smtp.mailtrap.io",
-    domain: "smtp.mailtrap.io",
-    port: "2525",
-    authentication: :cram_md5
-  }
 
+  config.action_mailer.smtp_settings = { address: "127.0.0.1", port: 1025 }
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
 end
