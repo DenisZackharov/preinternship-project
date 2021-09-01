3694f8c901162924e0aa048a88e9634f0c0611bc
Denis <78980031+DenisZackharov@users.noreply.github.com>
2021-09-01 13:34:51 +0300

    Update user_decorator.rb

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,7 +1,7 @@
 class UserDecorator < ApplicationDecorator
   delegate :id, :firstname, :lastname, :email
 
-  def firstname_lastname_with_email
+  def fullname_with_email
     "#{object.firstname} #{object.lastname} (#{object.email})"
   end
 end
