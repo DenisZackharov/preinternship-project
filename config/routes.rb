b7620e25e022d26f6d29a919926cda57a5158f89
DenisZackharov <denis.zaharov@flatstack.com>
2021-09-04 21:30:32 +0300

    add article model and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root "preinternship#home"
   devise_for :users
   get "preinternship/about"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :articles do
+    resources :comments, only: %i[create destroy]
+  end
+  
 end
