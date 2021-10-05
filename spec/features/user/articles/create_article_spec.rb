8fda9935e2445bd833dfd3767a5a96d9dfd8beaf
deniszackharov <denis.zaharov@flatstack.com>
2021-10-05 18:15:32 +0300

    update create article test

--- spec/features/user/articles/create_article_spec.rb
+++ spec/features/user/articles/create_article_spec.rb
@@ -11,13 +11,19 @@ feature "CreateArticle" do
     fill_form(:article, :new, title: "First article", content: "Hello")
     click_on "Create Article"
 
+    expect(page).to have_content("Article was successfully created.")
     expect(page).to have_content("First")
     expect(page).to have_button("Publish")
+
+    click_link "Back to drafted articles"
+    expect(page).to have_link("Denis : First article")
   end
 
   scenario "User create article with invalid data" do
     fill_form(:article, :new, title: "Fi", content: "Hello")
     click_on "Create Article"
-
+    
+    expect(page).to have_content("Article could not be created")
+    expect(page).to have_content("Title is too short (minimum is 3 characters)")
   end
 end
