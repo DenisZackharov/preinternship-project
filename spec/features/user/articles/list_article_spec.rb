ca48da2552dcf74e5496e18e623e2c95c492b43b
deniszackharov <denis.zaharov@flatstack.com>
2021-10-08 16:36:48 +0300

    Finish list of articles spec

--- spec/features/user/articles/list_article_spec.rb
+++ spec/features/user/articles/list_article_spec.rb
@@ -1,23 +1,52 @@
 require "rails_helper"
 
 feature "ListArticle" do
-  let!(:user1) { create :user,  firstname: "Den" }
-  let!(:user2) { create :user,  firstname: "Gleb" }
-  let!(:user3) { create :user,  firstname: "Emil" }
+
+  before do
+    create :user,  firstname: "Timur", id: 2
+    create :user,  firstname: "Gleb", id: 3
+  end
+
+  let!(:current_user) { create :user, email: "denis.zaharov@flatstack.com", password: "100100", firstname: "Denis", lastname: "Zaharov", id: 1 }
 
   let!(:article1) { create :article, title: "First public", status: "public", user_id: 1 }
   let!(:article2) { create :article, title: "Second drafted", status: "draft", user_id: 1 }
   let!(:article3) { create :article, title: "Third public", status: "public", user_id: 2 }
   let!(:article4) { create :article, title: "Fourth public", status: "public", user_id: 3 }
-  let!(:article5) { create :article, title: "Fifth archived", status: "archive", user_id: 1 }
+  let!(:article5) { create :article, title: "Fifth drafted", status: "draft", user_id: 1 }
   let!(:article6) { create :article, title: "Sixth public", status: "public", user_id: 1 }
 
   scenario "Guest see only public article" do
     visit articles_path
 
-    expect(page).to have_content("Den : First public")
-    expect(page).to have_content("Gleb : Third public")
-    expect(page).to have_content("Emil : Fourth public")
-    expect(page).to have_content("Den : Sixth public")
+    expect(page).to have_content("Denis : First public")
+    expect(page).to have_content("Timur : Third public")
+    expect(page).to have_content("Gleb : Fourth public")
+    expect(page).to have_content("Denis : Sixth public")
+    expect(page).to_not have_content("Denis : Second drafted")
+    expect(page).to_not have_content("Denis : Fifth drafted")
+  end
+
+  scenario "User see himself public article in My articles" do
+    login_as current_user
+    visit user_articles_path
+
+    expect(page).to have_content("Denis : First public")
+    expect(page).to_not have_content("Denis : Second drafted")
+    expect(page).to_not have_content("Timur : Third public")
+    expect(page).to_not have_content("Gleb : Fourth public")
+    expect(page).to_not have_content("Denis : Fifth drafted")
+    expect(page).to have_content("Denis : Sixth public")
+  end
+  scenario "User see himself drafted article in My articles drafted articles" do
+    login_as current_user
+    visit user_articles_path(status: :draft)
+
+    expect(page).to_not have_content("Denis : First public")
+    expect(page).to have_content("Denis : Second drafted")
+    expect(page).to_not have_content("Timur : Third public")
+    expect(page).to_not have_content("Gleb : Fourth public")
+    expect(page).to have_content("Denis : Fifth drafted")
+    expect(page).to_not have_content("Denis : Sixth public")
   end
 end
