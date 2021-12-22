cc065dd90b0f0a5adf47755cfef770780d9ad6d1
deniszackharov <denis.zaharov@flatstack.com>
2021-12-23 00:12:22 +0300

    change root path

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,7 +1,3 @@
 class UserDecorator < ApplicationDecorator
   delegate :id, :firstname, :lastname, :email
-
-  def fullname_with_email
-    "#{object.firstname} #{object.lastname} (#{object.email})"
-  end
 end
