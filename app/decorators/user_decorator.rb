dc26e338cf079a7571ac5939d0165d3da0f20828
deniszackharov <denis.zaharov@flatstack.com>
2022-01-30 22:46:28 +0300

    lil fix

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,9 +1,5 @@
 class UserDecorator < ApplicationDecorator
   delegate :id, :firstname, :lastname, :email
-  
-  def fullname_with_email
-    "#{object.firstname} #{object.lastname} (#{object.email})"
-  end
 
   def fullname
     "#{object.firstname} #{object.lastname}"
